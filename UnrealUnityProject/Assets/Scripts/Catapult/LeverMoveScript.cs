cb6559acbe4111e36853fe9e3d51a9d9df32d2ce
SnipMonk <sniper.monk.batler@gmail.com>
2023-02-20 17:56:20 +1000

    LEVERE REOWRK
    KILL ME PLS

--- UnrealUnityProject/Assets/Scripts/Catapult/LeverMoveScript.cs
+++ UnrealUnityProject/Assets/Scripts/Catapult/LeverMoveScript.cs
@@ -6,6 +6,7 @@ public class LeverMoveScript : MonoBehaviour
 {
     [SerializeField] HingeJoint hinge;
     [SerializeField] Transform linearComp;
+    [SerializeField] Transform endPoint;
     [SerializeField] Transform pointrer;
 
     [SerializeField] float minXCord;
@@ -13,6 +14,7 @@ public class LeverMoveScript : MonoBehaviour
 
     [SerializeField] float prog;
 
+    [SerializeField] Vector3 debug;
     private float minAngle;
     private float maxAngle;
     private JointSpring hingeJS;
@@ -23,21 +25,73 @@ public class LeverMoveScript : MonoBehaviour
         maxAngle = hinge.limits.max;
         hingeJS = hinge.spring;
     }
-    private void Update()
+
+    private Vector3 projectedPont(Vector3 pos)
+    {
+        return Vector3.Project(pointrer.position - linearComp.position, linearComp.right) + linearComp.position;
+    }
+
+    private float clampedDist()
+    {
+        Vector3 projcted = projectedPont(pointrer.position);
+        Vector3 local = linearComp.InverseTransformPoint(projcted);
+
+        float dist = (local).magnitude;
+        dist *= Mathf.Sign(Vector3.Dot(Vector3.right, local));
+
+        debug = Vector3.one * dist;
+
+        dist = Mathf.Max(
+            Mathf.Min(
+                dist,
+                maxXCord
+                ),
+            minXCord
+        );
+
+        return dist;
+    }
+
+    private Vector3 clampPos()
     {
 
-        float newX = Mathf.Min(
-                    Mathf.Max(
-                        Vector3.Project(pointrer.localPosition, linearComp.right).x,
-                        minXCord),
-                    maxXCord
-                );
-        float prec = 1 - (newX - minXCord ) / (maxXCord - minXCord) ;
-        
+        Vector3 projcted = projectedPont(pointrer.position);
+        Vector3 local = linearComp.InverseTransformPoint(projcted);
 
-        hingeJS.targetPosition = Mathf.Lerp(minAngle, maxAngle, prec);
+        local = Vector3.right * clampedDist();
+        return local;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        
+        Gizmos.DrawSphere(linearComp.position, 0.13f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(pointrer.position, 0.12f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(projectedPont(pointrer.position), 0.1f);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(linearComp.TransformPoint(clampPos()), 0.2f) ;
+    }
+
+    private void Update()
+    {
+        prog = 1 - (clampedDist() - minXCord) / (maxXCord - minXCord);
+        hingeJS.targetPosition = Mathf.Lerp(minAngle, maxAngle, prog);
         hinge.spring = hingeJS;
-        prog = Mathf.Lerp(minAngle, maxAngle, prec);
+    }
+
+    public void ResetPointer()
+    {
+        pointrer.position = endPoint.position;
+        pointrer.rotation = endPoint.rotation;
+        // Vector3 projected = projectedPont(linearComp.InverseTransformPoint(pointrer.position));
+        // projected.x = calcNewX() - minXCord;
+        // pointrer.position = linearComp.TransformPoint(projected);
     }
 
 }
